05e735924fb20c752c322cc78524613d16c10057
Akash Islam <akzzislam19194@gmail.com>
2023-09-16 22:34:35 +0600

    AuthProvider Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <section className="flex-start flex-col padding mb-16">
+        <h1>Categories</h1>
+        <h1>Banner</h1>
+        <h1>Posts</h1>
+        <h1>LoadMore</h1>
+      </section>
     </div>
   );
 };
